0d69f2fe1376b244772e4dbf517061bb40efc863
Sim <simmatrix100@gmail.com>
2020-12-16 03:55:14 +0800

    feat: Add core functionalities for Robot class

--- src/core/Robot.ts
+++ src/core/Robot.ts
@@ -1,12 +1,133 @@
+import IncorrectInfoError from '../errors/IncorrectInfoError';
+import InsufficientInfoError from '../errors/InsufficientInfoError';
+
+export enum RobotDirection {
+  NORTH = 'NORTH',
+  SOUTH = 'SOUTH',
+  EAST = 'EAST',
+  WEST = 'WEST',
+}
+
+export enum RobotRotation {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
 class Robot {
   private isSafeMode: boolean = true;
+  private positions: Coordinate = { x: 0, y: 0 };
+  private direction: RobotDirection = RobotDirection.NORTH;
+  private dimensions: Coordinate = { x: 5, y: 5 };
 
   setSafeMode(isSafeMode: boolean) {
     this.isSafeMode = isSafeMode;
   }
 
+  setDimensions(dimensions: Coordinate = { x: 5, y: 5 }) {
+    this.dimensions = dimensions;
+  }
+
   getSafeMode() {
     return this.isSafeMode;
+  }
+
+  getPosition() {
+    return this.positions;
+  }
+
+  getDirection() {
+    return this.direction;
+  }
+
+  report() {
+    return `${Object.values(this.getPosition()).join(',')},${this.getDirection()}`;
+  }
+
+  parseInput(placement: string): { positions: Coordinate; direction: RobotDirection } | Error {
+    try {
+      const _placement = placement.split(',').map((p) => p.trim());
+      if (_placement.length < 3) throw new InsufficientInfoError();
+      if (
+        isNaN(parseInt(_placement[0])) ||
+        isNaN(parseInt(_placement[0])) ||
+        !Object.values(RobotDirection).includes(_placement[2] as RobotDirection)
+      )
+        throw new IncorrectInfoError();
+
+      return {
+        positions: { x: parseInt(_placement[0]), y: parseInt(_placement[1]) } as Coordinate,
+        direction: _placement[2] as RobotDirection,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  place(placement: string) {
+    try {
+      const parsedInput = this.parseInput(placement) as { positions: Coordinate; direction: RobotDirection };
+      this.positions = parsedInput.positions;
+      this.direction = parsedInput.direction;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  move(step: number) {
+    let newPositionY = 0;
+    let newPositionX = 0;
+
+    switch (this.direction) {
+      case RobotDirection.NORTH:
+        newPositionY = this.positions.y + step;
+        if (newPositionY > this.dimensions.y) newPositionY = this.dimensions.y;
+        this.positions = { ...this.positions, y: newPositionY };
+        break;
+      case RobotDirection.SOUTH:
+        newPositionY = this.positions.y - step;
+        if (newPositionY < 0) newPositionY = 0;
+        this.positions = { ...this.positions, y: newPositionY };
+        break;
+      case RobotDirection.EAST:
+        newPositionX = this.positions.x + step;
+        if (newPositionX > this.dimensions.x) newPositionX = this.dimensions.x;
+        this.positions = { ...this.positions, x: newPositionX };
+        break;
+      case RobotDirection.WEST:
+        newPositionX = this.positions.x - step;
+        if (newPositionX < 0) newPositionX = 0;
+        this.positions = { ...this.positions, x: newPositionX };
+        break;
+      default:
+        throw new IncorrectInfoError();
+    }
+  }
+
+  rotate(rotation: string) {
+    const directions = [RobotDirection.NORTH, RobotDirection.EAST, RobotDirection.SOUTH, RobotDirection.WEST];
+    let currentDirectionIndex = directions.indexOf(this.direction);
+
+    switch (rotation) {
+      case RobotRotation.LEFT:
+        currentDirectionIndex--;
+        break;
+      case RobotRotation.RIGHT:
+        currentDirectionIndex++;
+        break;
+    }
+
+    if (currentDirectionIndex < 0) {
+      this.direction = RobotDirection.WEST;
+    } else if (currentDirectionIndex > 3) {
+      this.direction = RobotDirection.NORTH;
+    } else {
+      this.direction = directions[currentDirectionIndex];
+    }
   }
 }
 
--- test/robot.spec.js
+++ test/robot.spec.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 chai.should();
 chai.use(sinonChai);
 
-import Robot from '../src/core/Robot';
+import Robot, { RobotDirection, RobotRotation } from '../src/core/Robot';
 
 describe('RobotClass Test', function () {
   beforeEach(function () {
@@ -17,14 +17,86 @@ describe('RobotClass Test', function () {
     // sinon.verifyAndRestore();
   });
 
-  it('should be able to see default safe mode as on', function () {
-    const robot = new Robot();
-    expect(robot.getSafeMode()).to.be.true;
+  describe('General', function () {
+    it('should be able to see default safe mode as on', function () {
+      const robot = new Robot();
+      expect(robot.getSafeMode()).to.be.true;
+    });
+
+    it('should be able to turn off the safe mode', function () {
+      const robot = new Robot();
+      robot.setSafeMode(false);
+      expect(robot.getSafeMode()).to.be.false;
+    });
   });
 
-  it('should be able to turn off the safe mode', function () {
+  describe('Main', function () {
     const robot = new Robot();
-    robot.setSafeMode(false);
-    expect(robot.getSafeMode()).to.be.false;
-  })
+
+    it('should be able to get the default position', function () {
+      expect(robot.getPosition()).to.deep.equal({ x: 0, y: 0 });
+    });
+
+    it('should be able to get the default direction', function () {
+      expect(robot.getDirection()).to.equal(RobotDirection.NORTH);
+    });
+
+    it('should be able to parse user input', function () {
+      const placement = '2,3,SOUTH';
+      const parsedInput = robot.parseInput(placement);
+      expect(parsedInput.positions).to.deep.equal({ x: 2, y: 3 });
+      expect(parsedInput.direction).to.equal(RobotDirection.SOUTH);
+    });
+
+    it('should be throwing an error when giving insufficient placement info', function () {
+      const placement = '2,3';
+      expect(() => robot.parseInput(placement)).to.throw('Please specify both of the positions and direction');
+    });
+
+    it('should be throwing an error when giving incorrect placement info', function () {
+      const placement = '2,3,WEEEEEEEST';
+      expect(() => robot.parseInput(placement)).to.throw('Please specify a valid positions and direction');
+    });
+
+    it('report() - should be able to report the default position and direction in the required output format', function () {
+      expect(robot.report()).to.equal('0,0,NORTH');
+    });
+
+    it('place() - should be able to place at a specific coordinate', function () {
+      const placement = '1,2,EAST';
+      robot.place(placement);
+      expect(robot.report()).to.equal(placement);
+    });
+
+    it('move() - given it is now at the position of 0,0 and facing NORTH, should be able to move 2 steps up', function () {
+      const initialPlacement = '0,0,NORTH';
+      const newPlacement = '0,2,NORTH';
+      robot.place(initialPlacement);
+      robot.move(2);
+      expect(robot.report()).to.equal(newPlacement);
+    });
+
+    it('rotate() - given it is now facing NORTH, should be able to rotate to the right to face EAST', function () {
+      const initialPlacement = '0,0,NORTH';
+      const newPlacement = '0,0,EAST';
+      robot.place(initialPlacement);
+      robot.rotate(RobotRotation.RIGHT);
+      expect(robot.report()).to.equal(newPlacement);
+    });
+
+    it('rotate() - given it is now facing NORTH, should be able to rotate twice to the right to face SOUTH', function () {
+      const initialPlacement = '0,0,NORTH';
+      const newPlacement = '0,0,SOUTH';
+      robot.place(initialPlacement);
+      robot.rotate(RobotRotation.RIGHT);
+      robot.rotate(RobotRotation.RIGHT);
+      expect(robot.report()).to.equal(newPlacement);
+    });
+  });
+
+  describe('History', function () {
+    it('should be able to backup the current state', function () {});
+
+    it('should be able to restore to the previous state', function () {});
+  });
 });
